7b62dd9a0e7c5046780e1cbc06adffd5b26eab9d
PJSM <pjsm.lesi@gmail.com>
2024-07-10 18:34:47 +0100

    Severeal files added and updated

---
+++ 08_20240710/C05_ASP/D02_EF6_CodeFirst/Context/BlgContext.cs
@@ -0,0 +1,26 @@
+﻿using D02_EF6_CodeFirst.Class;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace D02_EF6_CodeFirst.Context
+{
+
+    internal class BlogContext : DbContext
+    {
+
+        // No construtor passamos o nome da connectionstring registada no App.config
+        public BlogContext() : base("BlogEntitiesCS") 
+        { }
+
+        // Método executado no startuo: criar tabelas, criar relações, criar constraints,chaves, ...
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Desativar a pluralização das tabelas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        // Dbsets (representação em memória das tabelas)
+        public virtual DbSet<Blog> Blog { get; set; }
+        public virtual DbSet<Post> Post { get; set; }
+    }
+}
