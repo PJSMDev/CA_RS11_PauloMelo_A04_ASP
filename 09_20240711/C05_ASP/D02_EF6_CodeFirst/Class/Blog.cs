93b20fac9a2e72dbe6e3ddd239b65711ee03a10f
PJSM <pjsm.lesi@gmail.com>
2024-07-15 18:28:20 +0100

    Added several files

---
+++ 09_20240711/C05_ASP/D02_EF6_CodeFirst/Class/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace D02_EF6_CodeFirst.Class
+{
+
+    internal class Blog
+    {
+
+        #region Properties
+
+        // Scalar Properties
+        public int BlogID { get; set; }
+
+        [Required]
+        //[Column(TypeName = "nvarchar(100)")]  // versão 4.7.2
+        [Column(TypeName = "nvarchar")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // Navigation Properties -> ligam as tabelas
+        // 1 blog tem uma coleção de posts (1 -> n)
+        public virtual ICollection<Post> Post {  get; set; } 
+
+        #endregion
+    }
+}
